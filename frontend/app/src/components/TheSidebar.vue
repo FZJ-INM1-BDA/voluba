04e86c0c881cfe0d0667afe8ff01b6103dace676
Xiao Gui <xgui3783@gmail.com>
2018-11-26 10:00:23 +0100

    feat: added animation on card hover + on card collapse

--- frontend/app/src/components/TheSidebar.vue
+++ frontend/app/src/components/TheSidebar.vue
@@ -1,65 +1,92 @@
 <template>
   <div id="sidebar">
     <div id="accordion" role="tablist">
-      <b-card no-body>
-        <b-card-header header-tag="header" role="tab">
-          <a v-b-toggle.collapse-datasets>
-            <font-awesome-icon icon="angle-left" pull="right" class="when-closed"/>
-            <font-awesome-icon icon="angle-down" pull="right" class="when-opened"/>
-            <h6><strong>Datasets</strong></h6>
-          </a>
-        </b-card-header>
-        <b-collapse id="collapse-datasets" visible>
-          <b-card-body>
-            <label>Select a reference volume:</label>
-            <br>
-            <select id="select-reference" v-model="selectReference">
-              <option v-for="referenceURL in referenceURLs" :key="referenceURL.id" :value="referenceURL.value">
-                {{ referenceURL.text }}
-              </option>
-            </select>
-            <hr>
-            <label>Select a template volume:</label>
-            <br>
-            <select id="select-template" v-model="selectTemplate">
-              <option v-for="templateURL in templateURLs" :key="templateURL.id" :value="templateURL.value">
-                {{ templateURL.text }}
-              </option>
-            </select>
-            <br><br>
-            <b-button href="#" variant="secondary">
-              <font-awesome-icon icon="upload"/>
-              Upload
-            </b-button>
-          </b-card-body>
-        </b-collapse>
-      </b-card>
 
-      <b-card no-body>
-        <b-card-header header-tag="header" role="tab">
-          <a v-b-toggle.collapse-registration>
-            <font-awesome-icon icon="angle-left" pull="right" class="when-closed"/>
-            <font-awesome-icon icon="angle-down" pull="right" class="when-opened"/>
-            <h6><strong>Registration</strong></h6>
-          </a>
-        </b-card-header>
-        <b-collapse id="collapse-registration">
-          <b-card-body></b-card-body>
-        </b-collapse>
-      </b-card>
+      <!-- Dataset -->
+      <card-component :initialVisibility = "true" :id = "'dataset'">
+        <template slot = "header">
+          <h6><strong>Dataset</strong></h6>
+        </template>
+        <template slot = "body">
+          <label>Select a reference volume:</label>
+          <br>
+          <select id="select-reference" v-model="selectReference">
+            <option v-for="referenceURL in referenceURLs" :key="referenceURL.id" :value="referenceURL.value">
+              {{ referenceURL.text }}
+            </option>
+          </select>
+          <hr>
+          <label>Select a template volume:</label>
+          <br>
+          <select id="select-template" v-model="selectTemplate">
+            <option v-for="templateURL in templateURLs" :key="templateURL.id" :value="templateURL.value">
+              {{ templateURL.text }}
+            </option>
+          </select>
+          <br><br>
+          <b-button href="#" variant="secondary">
+            <font-awesome-icon icon="upload"/>
+            Upload
+          </b-button>
+        </template>
+      </card-component>
 
-      <b-card no-body>
-        <b-card-header header-tag="header" role="tab">
-          <a v-b-toggle.collapse-landmark-pairs>
-            <font-awesome-icon icon="angle-left" pull="right" class="when-closed"/>
-            <font-awesome-icon icon="angle-down" pull="right" class="when-opened"/>
-            <h6><strong>Landmark pairs</strong></h6>
-          </a>
-        </b-card-header>
-        <b-collapse id="collapse-landmark-pairs">
+      <!-- Registration -->
+      <card-component :initialVisibility = "false" :id = "'registration'">
+        <template slot = "header">
+          <h6><strong>Registration</strong></h6>
+        </template>
+        <template slot = "body">
           <b-card-body></b-card-body>
-        </b-collapse>
-      </b-card>
+        </template>
+      </card-component>
+
+      <!-- Landmark Pairs -->
+      <card-component :initialVisibility = "false" :id = "'landmarkPairs'">
+        <template slot = "header">
+          <h6><strong>Landmark pairs</strong></h6>
+        </template>
+        <template slot = "body">
+          <b-card-body></b-card-body>
+        </template>
+      </card-component>
+
+      <!-- Transformation -->
+      <card-component :initialVisibility = "false" :id = "'transformation'">
+        <template slot = "header">
+          <h6><strong>Transformation</strong></h6>
+        </template>
+        <template slot = "body">
+          <label>Select a transformation type:</label>
+          <br>
+          <select id="select-transformation" v-model="selectTransformation">
+            <option v-for="transformationType in transformationTypes" :key="transformationType.id"
+                    :value="transformationType.value">
+              {{ transformationType.text }}
+            </option>
+          </select>
+          <br><br>
+          <b-button href="#" variant="secondary">
+            <font-awesome-icon icon="play-circle"/>
+            Compute transformation
+          </b-button>
+          <hr>
+          <b-container fluid>
+            <b-row>
+              <b-col md="5"><label>Determinant:</label></b-col>
+              <b-col md="7"><label/></b-col>
+            </b-row>
+            <b-row class="my-1">
+              <b-col md="5"><label>RMSE:</label></b-col>
+              <b-col md="7"><label/></b-col>
+            </b-row>
+          </b-container>
+          <b-button href="#" variant="secondary">
+            <font-awesome-icon icon="eye"/>
+            Show transformation matrix
+          </b-button>
+        </template>
+      </card-component>
 
       <b-card no-body>
         <b-card-header header-tag="header" role="tab">
@@ -107,6 +134,7 @@
 </template>
 
 <script>
+import CardComponent from '@/components/Card'
 export default {
   name: 'SidebarComponent',
   data () {
@@ -130,7 +158,10 @@ export default {
       selectTransformation: 'rigid'
     }
   },
-  methods: {}
+  methods: {},
+  components: {
+    CardComponent
+  }
 }
 </script>
 
