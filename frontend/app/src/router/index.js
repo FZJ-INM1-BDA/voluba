cb6be5f6e12ec77b486639757f5a47f6239c4788
Xiao Gui <xgui3783@gmail.com>
2019-01-21 12:04:55 +0100

    feat: build frontend in docker

--- frontend/app/src/router/index.js
+++ frontend/app/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Step1 from '@/components/toolbars/ToolbarStep1'
+import Step2 from '@/components/toolbars/ToolbarStep2'
+import Step3 from '@/components/toolbars/ToolbarStep3'
+import NotFound from '@/views/NotFound'
+
 Vue.use(Router)
 
 export default new Router({
@@ -14,26 +19,26 @@ export default new Router({
       name: 'Step 1',
       shown: true,
       displayName: 'Data Selection & 3D Anchoring',
-      component: () => import('@/components/toolbars/ToolbarStep1')
+      component: Step1
     },
     {
       path: '/step2',
       name: 'Step 2',
       shown: true,
       displayName: 'Entering Landmark-Pairs',
-      component: () => import('@/components/toolbars/ToolbarStep2')
+      component: Step2
     },
     {
       path: '/step3',
       name: 'Step 3',
       shown: true,
       displayName: 'Save & Export Results',
-      component: () => import('@/components/toolbars/ToolbarStep3')
+      component: Step3
     },
     {
       path: '*',
       name: 'NotFound',
-      component: () => import('@/views/NotFound')
+      component: NotFound
     }
   ],
   mode: 'hash'
