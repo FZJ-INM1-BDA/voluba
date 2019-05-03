8f543aaa3065ce39f2a10739618aabef66d2f58f
Xiao Gui <xgui3783@gmail.com>
2019-05-03 17:56:12 +0200

    feat(WIP): csp

--- frontend/deploy/server/index.js
+++ frontend/deploy/server/index.js
@@ -3,11 +3,41 @@ const session = require('express-session')
 const path = require('path')
 const dotenv = require('dotenv')
 const MemoryStore = require('memorystore')(session)
+const csp = require('helmet-csp')
+const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3000
 const SESSIONSECRET = process.env.SESSIONSECRET || `i will never love carrots`
+const CSP_DEFAULT_SRC = process.env.CSP_DEFAULT_SRC && process.env.CSP_DEFAULT_SRC.split(';') || []
+const CSP_CONNECT_SRC = process.env.CSP_CONNECT_SRC && process.env.CSP_CONNECT_SRC.split(';') || []
 
 const app = express()
+
+app.use(csp({
+  directives: {
+    styleSrc: ["'unsafe-inline'", "'self'"],
+    connectSrc: ["'self'", '*.humanbrainproject.org', '*.humanbrainproject.eu', '*.hbp.eu', ...CSP_CONNECT_SRC],
+    defaultSrc: ["'self'", ...CSP_DEFAULT_SRC],
+    reportUri: '/report-violation'
+  },
+  loose: false,
+  reportOnly: true
+}))
+
+app.post('/report-violation', bodyParser.json({
+  type: ['json', 'application/csp-report']
+}), (req, res) => {
+  if (req.body) {
+    /**
+     * Handle CSP Violation
+     */
+    console.log('CSP Violation: ', req.body)
+  } else {
+    console.log('CSP Violation: No data received!')
+  }
+  res.status(204).end()
+})
+
 dotenv.config()
 
 const store = new MemoryStore({
