7b8e3a971b804ca5e5b45f9f9b1173edeb0d6be4
Xiao Gui <xgui3783@gmail.com>
2019-06-19 11:34:22 +0200

    bugfix: oidc timeout

--- frontend/deploy/server/strategies/oidc.js
+++ frontend/deploy/server/strategies/oidc.js
@@ -1,10 +1,21 @@
-const { Issuer, Strategy } = require('openid-client')
+const { Issuer, Strategy, custom } = require('openid-client')
 
 const defaultCb = (tokenset, {id, ...rest}, done) => {
   return done(null, {
     id: id || Date.now(),
     ...rest
   })
+}
+
+/**
+ * timeout happens way too often
+ * increase timeout threshold and introduce retries according to
+ * https://github.com/panva/node-openid-client/blob/master/docs/README.md#customizing-http-requests
+ */
+const overWritingDefaultGotOption = (options) => {
+  options.timeout = 5000
+  options.retry = 3
+  return options
 }
 
 exports.configureAuth = async ({ discoveryUrl, clientId, clientSecret, redirectUri, clientConfig = {}, cb = defaultCb }) => {
@@ -21,12 +32,14 @@ exports.configureAuth = async ({ discoveryUrl, clientId, clientSecret, redirectU
     throw new Error('redirectUri must be defined!')
 
   const issuer = await Issuer.discover(discoveryUrl)
+  issuer[custom.http_options] = overWritingDefaultGotOption
 
   const client = new issuer.Client({
     client_id: clientId,
     client_secret: clientSecret,
     ...clientConfig
   })
+  client[custom.http_options] = overWritingDefaultGotOption
 
   const oidcStrategy = new Strategy({
     client,
