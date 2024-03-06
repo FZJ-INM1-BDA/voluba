ccdc3b823f05507424aa2fe0d822c61178385795
Xiao Gui <xgui3783@gmail.com>
2024-03-06 17:05:00 +0100

    fix translate

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -252,12 +252,11 @@ const getStore = ({ user = null, experimentalFeatures = {} } = {}) => new Vuex.S
       output[13] = nm[1]
       output[14] = nm[2]
 
-      const { mat4 } = window.export_nehuba
-      mat4.transpose(output, output)
+      const transform = [0,1,2].map(r => [0,1,2,3].map(c => output[ c * 4 + r ])).reduce((acc, curr) => [...acc, ...curr], []).join(",")
 
       pluginUrl.searchParams.set(
         "transform", 
-        [0,1,2].map(r => [0,1,2,3].map(c => output[ c * 4 + r ])).reduce((acc, curr) => [...acc, ...curr], []).join(",")
+        transform
       )
 
       const pluginUrlString = pluginUrl.toString()
