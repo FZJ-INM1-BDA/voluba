27e20df529d3215a29ba02934567f142a03abf71
Xiao Gui <xgui3783@gmail.com>
2025-06-04 14:09:40 +0200

    feat: added indication when add landmark mode is toggled on fix: ctrl c now properly copy highlighted text fix: secondary viewer now properly allow reposition landmarks & no longer flicker landmark when attempting to place the landmark

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,68 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Inject, inject } from '@angular/core';
+import { TextOnlySnackBar } from '@angular/material/snack-bar';
 import { select, Store } from '@ngrx/store';
+import { distinctUntilChanged, takeUntil } from 'rxjs';
 import { User } from 'src/const';
+import { MatSnackBar, MatSnackBarRef } from 'src/sharedModule';
 import * as app from 'src/state/app';
+import { DestroyDirective } from 'src/util/destroy.directive';
 
 @Component({
   selector: 'voluba-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  hostDirectives: [
+    DestroyDirective
+  ]
 })
 export class AppComponent {
+
+  #destroyed$ = inject(DestroyDirective).destroyed$
+
   currentStage$ = this.store$.pipe(select(app.selectors.stage));
 
   STAGE = app.consts.STAGE;
 
-  constructor(private store$: Store) {
+  #lmSnackBarRef: MatSnackBarRef<TextOnlySnackBar>|null = null
+
+  constructor(private store$: Store, private snackbar: MatSnackBar, @Inject(DOCUMENT) document: Document) {
+
+    document.addEventListener('copy', ev => {
+      ev.stopPropagation()
+    }, { capture: true })
+
     this.#fetchUser()
+
+    this.store$.pipe(
+      select(app.selectors.addLmMode),
+      distinctUntilChanged(),
+      takeUntil(this.#destroyed$)
+    ).subscribe(flag => {
+      if (!flag) {
+        if (this.#lmSnackBarRef) {
+          this.#lmSnackBarRef.dismiss()
+          this.#lmSnackBarRef = null
+        }
+        return
+      }
+
+      this.#lmSnackBarRef = this.snackbar.open(
+        `Adding landmarks`,
+        "Dismiss",
+        {
+          duration: -1
+        }
+      )
+      this.#lmSnackBarRef.onAction().subscribe(() => {
+        this.store$.dispatch(
+          app.actions.setAddLandmarkMode({
+            mode: false
+          })
+        )
+      })
+    })
   }
 
   async #fetchUser(){
